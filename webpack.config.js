bb3ce639d6df87cd115937db2596ce32210445cf
tap-sakamoto <sakamoto@fb.tapweb.jp>
2023-05-24 12:09:58 +0900

    use webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+    }
 };
