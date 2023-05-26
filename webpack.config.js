855e14fbe37ca178b4d893b069bd86e6ecedb219
tap-sakamoto <sakamoto@fb.tapweb.jp>
2023-05-26 15:21:35 +0900

    use bootstrap progress bar

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'index.html',
+        template: 'src/index.html'
+    })],
     module: {
         rules: [
             {
@@ -19,6 +24,10 @@ module.exports = {
                     // Compiles Sass to CSS
                     "sass-loader",
                 ],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
             },
             {
                 test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
